f53252462814ebe08d2b017acdcb96636abb19b4
shubhang <shubhang.chakrawarty@gmail.com>
2024-04-28 02:02:45 +0530

    temp commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Manga Mania',
+      title: 'Manga Man',
       theme: ThemeData(
         useMaterial3: true,
         brightness: Brightness.dark,
