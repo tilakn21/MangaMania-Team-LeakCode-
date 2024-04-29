5ba80f9265416008a806db01273b2b91b704ce9f
shubhang <shubhang.chakrawarty@gmail.com>
2024-04-29 09:39:42 +0530

    nsfw_CLassifier

--- NSFW_Classifier.py
+++ NSFW_Classifier.py
@@ -2,25 +2,42 @@ from PIL import Image
 import os
 from transformers import AutoModelForImageClassification, ViTImageProcessor
 import torch
+from transformers import pipeline
 
-folder = "/content/test mangas"
+folder = "/content/rando/t2"
 
-# Initialize the model and processor
 model = AutoModelForImageClassification.from_pretrained("Falconsai/nsfw_image_detection")
 processor = ViTImageProcessor.from_pretrained('Falconsai/nsfw_image_detection')
+
+nsfw_found = False 
 
 for root, dirs, files in os.walk(folder):
     for file_name in files:
         file_path = os.path.join(root, file_name)
-        
-        img = Image.open(file_path)
 
-        # Using the model directly
+        img = Image.open(file_path)
+        low_res_img = img.convert(mode='RGB')
+     #   low_res_img = img.resize((224,224))
+        classifier = pipeline("image-classification", model="Falconsai/nsfw_image_detection")
+        classifier(low_res_img)
+        
+
+        img = Image.open(file_path)
+        low_res_img = img.convert(mode='RGB')
+     #   low_res_img = img.resize((224,224))
         with torch.no_grad():
-            inputs = processor(images=img, return_tensors="pt")
+            inputs = processor(images=low_res_img, return_tensors="pt")
             outputs = model(**inputs)
             logits = outputs.logits
 
         predicted_label = logits.argmax(-1).item()
         label = model.config.id2label[predicted_label]
-        print(f"File: {file_path} - Predicted label: {label}")
+     
+        if label == 'nsfw':
+            nsfw_found = True
+            
+            break
+if(nsfw_found):
+  print(1)  
+else:
+  print(0)
